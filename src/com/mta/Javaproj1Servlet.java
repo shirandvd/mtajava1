5d1b7e5db94dcecdf8fe1fe662f0ae9adc451d38
Shiran Davidi <shirandvd1@gmail.com>
2014-11-09 23:44:15 +0200

    result of

--- src/com/mta/Javaproj1Servlet.java
+++ src/com/mta/Javaproj1Servlet.java
@@ -8,6 +8,13 @@ public class Javaproj1Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/html");
-		resp.getWriter().println("<h1>Hello, MY world !!!</h1>");
+		resp.getWriter().println("<h1>Hello, MY world</h1>");
+		int num1=4;
+		int num2=3;
+		int num3=7;
+		int result=(num1+num2)*num3;
+		String resultStr = new String("<h1>Result of"+"("+num1+"+"+num2+")"+"*"+num3+"="+result+"</h1>");
+		resp.getWriter().println(resultStr);
+				
 	}
 }
