bbf3d25f6a8cfdacd4f2fb3b0fed52fbab5b78ec
Shiran Davidi <shirandvd1@gmail.com>
2014-11-23 19:40:24 +0200

    exercise04.stock

--- src/com/mta/Stock.java
+++ src/com/mta/Stock.java
@@ -51,41 +51,4 @@ public class Stock extends HttpServlet {
 		String stockHtmlDetailsString = "<b>Stock symbol: </b>"+getSymbol()+"<b> Ask: </b>"+getAsk()+"<b> Bid: </b>"+getBid()+"<b> Date: </b>" +getDate();
 		return stockHtmlDetailsString;
 	}
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/html");
-		
-		Stock s1 = new Stock();
-		Stock s2 = new Stock();
-		Stock s3 = new Stock();
-		
-		java.util.Date date = new java.util.Date();
-		date.setYear(114);
-		date.setMonth(10);
-		date.setDate(15);
-		date.setHours(0);
-		date.setMinutes(0);
-		date.setSeconds(0);
-		
-		s1.setSymbol("PIH");
-		s1.setAsk(12.4f);
-		s1.setBid(13.1f);
-		s1.setdate(date);
-		
-		s2.setSymbol("AAL");
-		s2.setAsk(5.5f);
-		s2.setBid(5.78f);
-		s2.setdate(date);
-		
-		s3.setSymbol("CAAS");
-		s3.setAsk(31.5f);
-		s3.setBid(31.2f);
-		s3.setdate(date);
-		
-		String s1Str=s1.getHtmlDescription();
-		String s2Str=s2.getHtmlDescription();
-		String s3Str=s3.getHtmlDescription();
-		
-		resp.setContentType("text/html");
-		resp.getWriter().println(s1Str+"<br>"+s2Str+"<br>"+s3Str);
-	}
 }
